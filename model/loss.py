203b33ab8480f5e31b8582efb8d7df6dfc12a5d7
qtw1998 <qtw1998@163.com>
2021-10-10 21:12:39 +0800

    can be trained

--- model/loss.py
+++ model/loss.py
@@ -6,6 +6,4 @@ def nll_loss(output, target):
 
 
 def mse_loss(output, target):
-    print("output shape: {}".format(output.shape))
-    print("target shape: {}".format(target.shape))
     return F.mse_loss(output, target)
